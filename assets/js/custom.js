aebab49c4eb7212947abab716894b48b8a752628
HarshRadialCode <harshradialcode@gmail.com>
2022-03-10 13:37:33 +0530

    creating slider on overflow section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,43 @@
 // console.log(4);
+$('.post-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    centerMode: false,
+    autoplaySpeed: 100,
+    pauseOnDotsHover: true,
+    ltr: true,
+    // ltr: true,
+    // slidesToScroll: -1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
